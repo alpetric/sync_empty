9d07a7fe279097e0367364a7487bbf435a780e5b
Alexander Petric <alex@windmill.dev>
2025-07-24 19:43:42 +0000

    revert

--- f/test/hello3.ts
+++
@@ -1,8 +0,0 @@
-import '@slack/web-api'
-import * as wmill from 'windmill-client';
-
-export async function main() {
-  const { token } = await wmill.getResource('u/alex/test1234')
-  console.log('ta', token)
-  return "Hello world";
-}
